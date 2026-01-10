c337a27bc0399cbdb37d361858ca711da9749460
Daniil <misvakoff@gmail.com>
2026-01-10 18:58:15 +0500

    feat(admin): add stock filter and reset stock action

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-
 from shop.models import Product, ProductImage, Attribute
+from shop.filters import StockFilter
 
+# @admin.action(description="Сбросить остатки до 0")
+# def reset_stock(model_admin, request, queryset):
+#     updated_count = queryset.update(stock=0)
+#     model_admin.message_user(request, f"Остатки сброшены у {updated_count} товаров.")
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'stock', 'images', 'get_attributes')
+    list_filter = (StockFilter,)
+    search_fields = ('title', 'description')
+    actions = ("reset_stock",)    
+
+    @admin.action(description="Сбросить остатки до 0")
+    def reset_stock(self, request, queryset):
+        updated_count = queryset.update(stock=0)
+        self.message_user(request, f"Остатки сброшены у {updated_count} товаров.")
 
     @admin.display(description='Изображение')
     def images(self, obj: Product):

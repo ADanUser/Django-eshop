544dad650b49afb090a9ad93f1f60581720c22f9
Daniil <misvakoff@gmail.com>
2026-01-11 16:27:03 +0500

    feat: implement products view with template and timestamp

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,13 @@
+from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from shop.models import Product
+from datetime import datetime
 
 
 def all_products(request: HttpRequest) -> HttpResponse:
     products = Product.objects.all()
-    html_content = "<h1>Список всех продуктов</h1><ul>"
-    for product in products:
-        html_content += f"<li>{product.title} — {product.price}</li>"
-    html_content += "</ul>"
-    return HttpResponse(html_content)
-
-# Create your views here.
+    context = {
+        'products': products,
+        'current_time': datetime.now(),
+    }
+    return render(request, 'products.html', context)

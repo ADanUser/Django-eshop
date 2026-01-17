4bcc8dbc207f9bd1c6f836552ea4afe6541f24a1
Daniil <misvakoff@gmail.com>
2026-01-17 17:12:16 +0500

    feat(mixins): add authentication check and redirect to login

--- shop/mixins.py
+++ shop/mixins.py
@@ -1,6 +1,16 @@
 from datetime import datetime
 
+from django.shortcuts import redirect
+
+
 class IsAuthenticatedMixin:
+    login_url = "login"
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect(self.login_url)
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["current_time"] = datetime.now()
